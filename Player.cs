e794b48fda97abe97c0571e2d86b72b5b7d99b90
Nedim Basic <basicn86@gmail.com>
2024-05-12 22:32:42 -0400

    added player movement

---
+++ Player.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class Player : RigidBody3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Vector3 input = Vector3.Zero;
+		input.X = Input.GetAxis("move_left", "move_right");
+		input.Z = Input.GetAxis("move_forward", "move_back");
+
+		input = input.Normalized();
+
+		ApplyCentralForce(input * 1000f * (float)delta);
+	}
+}
