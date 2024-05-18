2d167aeaf0ef2f18d3e1ac90c9e25c288fb6a1a0
Nedim Basic <basicn86@gmail.com>
2024-05-18 15:32:42 -0400

    added mouse capturing

--- Player.cs
+++ Player.cs
@@ -6,17 +6,23 @@ public partial class Player : RigidBody3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Vector3 input = Vector3.Zero;
-		input.X = Input.GetAxis("move_left", "move_right");
-		input.Z = Input.GetAxis("move_forward", "move_back");
+		Vector3 moveVector = Vector3.Zero;
+		moveVector.X = Input.GetAxis("move_left", "move_right");
+		moveVector.Z = Input.GetAxis("move_forward", "move_back");
 
-		input = input.Normalized();
+		moveVector = moveVector.Normalized();
 
-		ApplyCentralForce(input * 2000f * (float)delta);
+		ApplyCentralForce(moveVector * 2000f * (float)delta);
+
+		if (Input.IsActionJustPressed("ui_cancel"))
+		{
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
 	}
 }
